e5e8b1404b4d6cd3f92b17b3cbd0340471141cf5
Tyler <tylergwlum@gmail.com>
2018-11-19 01:01:31 -0800

    Make grayscale copy of image

--- lanes.py
+++ lanes.py
@@ -1,5 +1,11 @@
 import cv2
+import numpy as np
 
 image = cv2.imread('test_image.jpg')
-cv2.imshow('result', image)
+
+## Make gray copy to not edit original
+lane_image = np.copy(image)
+gray = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
+
+cv2.imshow('result', gray)
 cv2.waitKey(0)

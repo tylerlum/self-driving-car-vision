cd70532bb4619ba784222f4400ee68ff602b2858
Tyler <tylergwlum@gmail.com>
2018-11-19 01:35:53 -0800

    Draw lane lines over original image

--- lanes.py
+++ lanes.py
@@ -15,6 +15,14 @@ def canny(image):
     canny = cv2.Canny(blur, 50, 150)
     return canny
     
+def display_lines(image, lines):
+    line_image = np.zeros_like(image)
+    if lines is not None:
+        for line in lines:
+             x1, y1, x2, y2 = line.reshape(4)
+             cv2.line(line_image, (x1, y1), (x2, y2), (255, 0, 0), 10)
+    return line_image
+
 def region_of_interest(image):
     height = image.shape[0]
     polygons = np.array([[(200, height), (1100, height), (550, 250)]])
@@ -30,5 +38,12 @@ lane_image = np.copy(image)
 canny = canny(lane_image)
 cropped_image = region_of_interest(canny)
 
-cv2.imshow("result", region_of_interest(cropped_image))
+## Detect lines in canny image
+lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100, np.array([]), minLineLength=40, maxLineGap=5)
+line_image = display_lines(lane_image, lines)
+
+## Put lines over lane image, weighted for clarity
+combo_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
+
+cv2.imshow("result", combo_image)
 cv2.waitKey(0)

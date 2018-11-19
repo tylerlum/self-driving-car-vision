7022aed36f2127f450561717fdbe698008811977
Tyler <tylergwlum@gmail.com>
2018-11-19 01:03:26 -0800

    Use Gaussian Blur

--- lanes.py
+++ lanes.py
@@ -7,5 +7,8 @@ image = cv2.imread('test_image.jpg')
 lane_image = np.copy(image)
 gray = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
 
-cv2.imshow('result', gray)
+## Use Gaussian blur method
+blur = cv2.GaussianBlur(gray, (5,5), 0)
+
+cv2.imshow('result', blur)
 cv2.waitKey(0)
